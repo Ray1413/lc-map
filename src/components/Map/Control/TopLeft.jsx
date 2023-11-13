91a71135dd14fe306db6e4e477b00ee554d942f6
ray <ray472809026@hotmail.com>
2023-11-13 16:40:48 +0800

    Comment some debug functions

--- src/components/Map/Control/TopLeft.jsx
+++ src/components/Map/Control/TopLeft.jsx
@@ -1,23 +1,22 @@
 import React from 'react'
 import Box from '@mui/material/Box'
-import useGlobalSetting from '@/hooks/useGlobalSetting'
-import { useParams } from 'next/navigation'
+// import useGlobalSetting from '@/hooks/useGlobalSetting'
+// import { useParams } from 'next/navigation'
 
 function TopLeft() {
   // const { resource } = useGlobalSetting()
-  const { selectedCategory } = useGlobalSetting()
-  const { language } = useParams()
+  // const { selectedCategory } = useGlobalSetting()
+  // const { language } = useParams()
   // console.log('TopLeft')
 
   return (
     <Box className="leaflet-top leaflet-left">
       <Box className="leaflet-control">
-        <div>
+        {/* <div>
           <div>Debug: </div>
           <div>{'Language: ' + language}</div>
-          {/* <div>{'selectedCategory: ' + selectedCategory}</div> */}
           <div>{'selectedCategory: ' + selectedCategory[language]}</div>
-        </div>
+        </div> */}
       </Box>
     </Box>
   )
